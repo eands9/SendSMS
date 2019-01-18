5383ef9df9e1db043ae2cdf306340c7508ea686b
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2019-01-18 09:28:45 -0600

    fixed

--- SendSMS/ViewController.swift
+++ SendSMS/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func pushBtn(_ sender: Any) {
+        let messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "This is a test!";
+        messageVC.recipients = ["469-910-6366"]
+        messageVC.messageComposeDelegate = self
+        
+        self.present(messageVC, animated: false, completion: nil)
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result) {
+        case .cancelled:
+            print("Message was cancelled")
+            dismiss(animated: true, completion: nil)
+        case .failed:
+            print("Message failed")
+            dismiss(animated: true, completion: nil)
+        case .sent:
+            print("Message was sent")
+            dismiss(animated: true, completion: nil)
+        default:
+            break
+        }
+    }
 }
 
