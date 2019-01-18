ef2a3b6c58db86d5d5b049f7b66886456a3e0815
Kyle Hernandez <kylehernandez@kyles-air.frontierlocal.net>
2019-01-18 07:25:58 -0600

    Initial Commit

---
+++ SendSMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendSMS
+//
+//  Created by Kyle Hernandez on 1/18/19.
+//  Copyright © 2019 Kyle Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
